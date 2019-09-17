ad076389c289ec14e464b48846b606c2a60ed38e
SeanPaulLevesque <41477212+SeanPaulLevesque@users.noreply.github.com>
2019-09-17 13:41:55 -0400

    Update ImageStitch.py

--- ImageStitch.py
+++ ImageStitch.py
@@ -6,14 +6,21 @@ from PIL import Image
 # scrape images from the website
 # file names are formatted "Zoom-Xcoord-Ycoord"
 # images are stored in seemingly random folders, can't figure it out, just check them all for every file
+
+# generate images list
+images = []
 for g in range(1,11):
     for x in range(1,54):
         for y in range(1,73):
-            filestring = "https://s3.eu-west-2.amazonaws.com/mapshow/Boston1/TileGroup" + str(g) + "/7-" + str(x) + "-" + str(y) + ".jpg"
-            try:
-                result = urllib.request.urlretrieve(filestring, "images/7-"+ str(x) + "-" + str(y) + ".jpg")
-            except IOError as e:
-                print('??????', e)
+            images.append("https://s3.eu-west-2.amazonaws.com/mapshow/Boston1/TileGroup" + str(g) + "/7-" + str(x) + "-" + str(y) + ".jpg")
+
+
+
+for url in images:
+    try:
+        result = urllib.request.urlretrieve(url, "images/7-"+ str(x) + "-" + str(y) + ".jpg")
+    except IOError as e:
+        print('??????', e)
 
 
 # set the size of the stitched together image
@@ -21,12 +28,12 @@ for x in range(100,1,-1):
     if path.exists("images/7-" + str(x) + "-1.jpg"):
         maxX = x - 1
         break
-        
+
 for y in range(100,1,-1):
     if path.exists("images/7-1-" + str(y) + ".jpg"):
         maxY = y - 1
         break
-        
+
 im = Image.open("images/7-2-2.jpg")
 (width, height) = im.size
 result_width = width * maxX
